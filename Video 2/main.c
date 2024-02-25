70bcbba4272df5fd4ec5fcf6b572928ba1d83be8
CodeWithArpit <150226804+arpitsharm@users.noreply.github.com>
2024-02-25 14:11:49 +0530

    Add files via upload

---
+++ Video 2/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    // this is ignore my compiler
+    /*
+    this is multi
+    line comment
+    */
+    // __CLR_OR_THIS_CALL
+    // __is_identifier
+    // _Maybe_raises_SEH_exception_
+    // _COM_Outptr_result_maybenull_
+    // int, char, float
+
+    // unsigned, double, long
+
+
+    unsigned  a = 22;
+    double b = 22.23;
+    // float b = 2.3;
+    char c = 'h';
+
+    printf("%d\n", a);
+    printf("%f\n", b);
+    printf("%c\n", c);
+
+    printf("Int Size :- %d\n", sizeof(int));
+    printf("Char Size :- %d\n", sizeof(char));
+    printf("Float Size :- %d\n", sizeof(float));
+    printf("unsigned int Size :- %d\n", sizeof(unsigned int));
+    printf("unsigned char Size :- %d\n", sizeof(unsigned char));
+    printf("unsigned Size :- %d\n", sizeof(unsigned));
+    printf("double Size :- %d\n", sizeof(double));
+    printf("long Size :- %d\n", sizeof(long));
+
+
+    // operators
+    // +, -, *, /
+
+
+
+
+    return 0;
+}
