fc9f22de26064836782ed14be42677a4476bf580
CodeWithArpit <150226804+arpitsharm@users.noreply.github.com>
2024-02-27 19:14:44 +0530

    Add files via upload

---
+++ Video 4/if_elseif.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a = 1;
+    
+    if (a == 10)
+    {
+        printf("a is 10 value bar\n");
+    }
+
+    else if (a > 10)
+    {
+        printf("a is greater than 10 of value bar");
+    }
+
+
+
+    return 0;
+}
---
+++ Video 4/if_elseif_else.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a = 1;
+    
+    if (a == 10)
+    {
+        printf("a is 10 value bar\n");
+    }
+
+    else if (a > 10)
+    {
+        printf("a is greater than 10 of value bar");
+    }
+
+
+    else
+    {
+        printf("Is a less than 10 and not equal to 10 of value bar");
+    }
+    
+
+    return 0;
+}
