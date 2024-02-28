af1bf847cd18de48ce307931be47fd695e7b3bf6
CodeWithArpit <150226804+arpitsharm@users.noreply.github.com>
2024-02-28 18:58:31 +0530

    Add files via upload

---
+++ Video 5/exersize.c
@@ -0,0 +1,39 @@
+// #include <stdio.h>
+
+// int main() {
+//     int value = 5;
+
+//     if (value == 1){
+//         printf("wow you are good");
+//     }
+
+//     else if (value == 2){
+//         printf("you are 18 year old");
+//     }
+
+//     else if (value == 3){
+//         printf("you are 50 year old");
+//     }
+
+//     else {
+//         printf("your choice is not good");
+//     }
+// }
+
+// // #include <stdio.h>
+
+// // int main() {
+// //     int a;
+// //     a = 3;
+// //     if (a == 1){
+// //         printf("wow you are good");
+// //     }
+
+// //     else if (a == 2){
+// //         printf("you are 18 year old");
+// //     }
+
+// //     else if (a == 3){
+// //         printf("you are 50 year old");
+// //     }
+// // }
